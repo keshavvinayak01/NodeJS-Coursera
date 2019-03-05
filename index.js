607c03c29aa09e223014c4ec52effcc56d04ffa4
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-03-05 20:51:14 +0530

    Fixed Typos

--- index.js
+++ index.js
@@ -5,31 +5,40 @@ const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion'
 
 MongoClient.connect(url).then((client) => {
-        assert.equal(err,null);
 
-        console.log('Connected corectly to server !');
+        console.log('Connected correctly to server');
         const db = client.db(dbname);
-
-        dboper.insertDocument(db,{name : "Vadonut", description : 'Test'}, 'dishes')
-        .then((result) => {
-                console.log('Insert Document:\n',result.ops);
-                return dboper.findDocuments(db,'dishes')})
-        .then((docs) => {
-                console.log('Found Document:\n',docs);
-                return dboper.updateDocument(db,{name:'Vadonut'},{description : 'Updated Test'},'dishes')})
-        
-        .then((result) => {
-                console.log('Updated Document :\n',result.result);
-                return dboper.findDocuments(db,'dishes')
-        })
-        .then((docs)  => {
-                console.log('Found Document:\n',docs)      ;
-                return db.dropCollection('dishes')
-        })
-        .then((result) => {
-                console.log("Dropped Collection : \n",result );
-                client.close();
-        })
-        .catch((err) => console.log(err));                                  
-})
-.catch((err) => console.log(err));
+    
+        dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+            "dishes")
+            .then((result) => {
+                console.log("Insert Document:\n", result.ops);
+    
+                return dboper.findDocuments(db, "dishes");
+            })
+            .then((docs) => {
+                console.log("Found Documents:\n", docs);
+    
+                return dboper.updateDocument(db, { name: "Vadonut" },
+                        { description: "Updated Test" }, "dishes");
+    
+            })
+            .then((result) => {
+                console.log("Updated Document:\n", result.result);
+    
+                return dboper.findDocuments(db, "dishes");
+            })
+            .then((docs) => {
+                console.log("Found Updated Documents:\n", docs);
+                                
+                return db.dropCollection("dishes");
+            })
+            .then((result) => {
+                console.log("Dropped Collection: ", result);
+    
+                return client.close();
+            })
+            .catch((err) => console.log(err));
+    
+    })
+    .catch((err) => console.log(err));
--- operations.js
+++ operations.js
@@ -1,21 +1,21 @@
 const assert = require('assert');
 
-exports.insertDocument = (db,document,collection,callback) => {
-        const coll = db.collection(collection);
-        return coll.insert(document);
+exports.insertDocument = (db, document, collection) => {
+    const coll = db.collection(collection);
+    return coll.insert(document);
 };
 
-exports.findDocuments = (db,collection,callback) => {
-        const coll = db.collection(collection);
-        return coll.find({}).toArray();
-}
-
-exports.removeDocument = (db,document,collection,callback) => {
-        const coll = db.collection(collection);
-        return coll.deleteOne(document);
+exports.findDocuments = (db, collection) => {
+    const coll = db.collection(collection);
+    return coll.find({}).toArray();
 };
 
-exports.updateDocument = (db,document,update,collection,callback) => {
-        const coll = db.collection(collection);
-        return coll.updateOne(document);
+exports.removeDocument = (db, document, collection) => {
+    const coll = db.collection(collection);
+    return coll.deleteOne(document);
+};
+
+exports.updateDocument = (db, document, update, collection) => {
+    const coll = db.collection(collection);
+    return coll.updateOne(document, { $set: update }, null);
 };
