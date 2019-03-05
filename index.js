e502aa291ddf465260b5aef96089785062c61e2e
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-03-05 19:56:16 +0530

    Node MongoDB operation-2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert');
-
+const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion'
 
@@ -8,24 +8,22 @@ MongoClient.connect(url,(err,client) => {
         assert.equal(err,null);
 
         console.log('Connected corectly to server !');
-
         const db = client.db(dbname);
-        const collection = db.collection('dishes');
 
-        collection.insertOne({ "name" : "Uthappizza", "description" : "test"},(err,result) => {
-                assert.equal(err,null);
-                console.log("After Insert : \n");
-                console.log(result.ops);
-
-                collection.find({}).toArray((err,docs) => {
-                        assert.equal(err,null);
-                        console.log('Found: \n');
-                        console.log(docs);
-
-                        db.dropCollection('dishes',(err,result) => {
-                                assert.equal(err,null);
-
-                                client.close();
+        dboper.insertDocument(db,{name : "Vadonut", description : 'Test'}, 'dishes',(result) => {
+                console.log('Insert Document:\n',result.ops);
+                dboper.findDocuments(db,'dishes',(docs) => {
+                        console.log('Found Document:\n',docs)
+                        dboper.updateDocument(db,{name:'Vadonut'},{description : 'Updated Test'},'dishes',
+                        (result) => {
+                                console.log('Updated Document :\n',result.result);
+                                dboper.findDocuments(db,'dishes',(docs) => {
+                                        console.log('Found Document:\n',docs)      ;
+                                        db.dropCollection('dishes',(result) => {
+                                                console.log("Dropped Collection : \n",result );
+                                                client.close();
+                                        });                                  
+                                });
                         });
                 });
         });
