f5e4a062df16abd5e4c45f9780540d0666559512
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-03-17 18:51:46 +0530

    ImageUpload over HTTPS

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var dishRouter  = require('./routes/dishRouter');
 var promoRouter  = require('./routes/promoRouter');
 var leaderRouter  = require('./routes/leaderRouter');
+var uploadRouter  = require('./routes/uploadRouter');
 var passport = require('passport');
 var authenticate = require('./authenticate');
 const mongoose = require('mongoose');
@@ -53,7 +54,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/leaders', leaderRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
-
+app.use('/imageUpload',uploadRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
